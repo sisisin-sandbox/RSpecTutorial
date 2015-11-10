c1eebb1267988a4cd863d1d90daab2a0a95ca629
sisisin <azsisisin@gmail.com>
2015-11-10 21:34:17 +0900

    refactor test. finished first iteration

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,14 +1,7 @@
 require_relative 'message_filter'
 
-describe MessageFilter do
-  before do
-    @filter = MessageFilter.new('foo')
-  end
-  it 'detects message with NG word' do
-    expect(@filter.detect?('hello from foo')).to eq true
-  end
-
-  it 'does not detect message without NG word' do
-    expect(@filter.detect?('hello world')).to eq false
-  end
+describe MessageFilter, 'with argument "foo"' do
+  subject { MessageFilter.new('foo') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello world') }
 end
