72145d1f717cf34bc789ad47d93cc642c0217f29
sisisin <azsisisin@gmail.com>
2015-11-14 08:55:28 +0900

    use have matcher

--- message_filter.rb
+++ message_filter.rb
@@ -2,7 +2,9 @@ class MessageFilter
   def initialize(*words)
     @ng_words = words
   end
+
   attr_reader :ng_words
+
   def detect?(text)
     @ng_words.any?{|w| text.include?(w)}
   end
--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'message_filter'
 require 'rspec/its'
+require 'rspec/collection_matchers'
 
 describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
@@ -11,6 +12,9 @@ describe MessageFilter do
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
+    it 'ng_words size is 1' do
+      expect(subject.ng_words).to have(1).item 
+    end
   end
   
   context 'with argument "foo","bar"' do
